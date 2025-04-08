f9a717450b9295aea6fb3bba9aca81d6e9a6e10f
Raluca <Pop.Va.Ra@student.utcluj.ro>
2025-04-08 16:12:13 +0300

    frontend by raluc

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { carsComponent } from './cars/cars.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes:
+  Routes = [
+  { path: 'cars', component: carsComponent },
+  { path: 'about', component: AboutComponent },
+];
+
