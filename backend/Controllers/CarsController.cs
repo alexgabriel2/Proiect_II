26811ebfb214ce7b0696dffe0bc7a460f87779ae
greenline <alexpascu2013@gmail.com>
2025-05-16 16:42:11 +0300

    updated car controller

--- backend/Controllers/CarsController.cs
+++ backend/Controllers/CarsController.cs
@@ -1,39 +1,55 @@
 ﻿using backend.Data;
 using backend.Entities;
+using backend.Models;
+using backend.services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using System.Security.Claims;
+//todo modify controller to use services
 namespace backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CarsController : ControllerBase
+    public class CarsController( ICarService _carService) : Controller
     {
-        private readonly AppDbContext _context;
-
-        public CarsController(AppDbContext context)
-        {
-            _context = context;
-        }
         [HttpGet]
-        public async Task<ActionResult<List<Car>>> GetCars()
-        {
-            var cars = await _context.Cars.ToListAsync();
-            return Ok(cars);
+        public async Task<ActionResult<List<CarDTO>>> GetCars() {
+            var cars = await _carService.GetCarsAsync();
+            var carDtos = cars.Select(car => new CarDTO {
+                Id = car.Id,
+                Make = car.Make,
+                Model = car.Model,
+                Year = car.Year,
+                Milleage = car.Milleage,
+                Price = car.Price,
+                FuelType = car.FuelType,
+                Status = car.Status,
+            }).ToList();
+            return Ok(carDtos);
         }
-        [HttpPost]
-        public async Task<ActionResult<List<Car>>> CreateCar([FromBody] Car car)
-        {
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Car>> GetCarById(Guid id) {
+            var car = await _carService.GetCarByIdAsync(id);
             if (car == null)
-            {
+                return NotFound();
+            return Ok(car);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Car>> CreateCar([FromBody] Car car) {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null) {
+                return Unauthorized("Invalid token");
+            }
+            if (car == null) {
                 return BadRequest("Car cannot be null");
             }
-            car.Id = Guid.NewGuid();
-            car.CreatedAt = DateTime.UtcNow.ToString("yyyy-MM-dd");
-            _context.Cars.Add(car);                 
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetCars), new { id = car.Id }, car);
+            var createdCar = await _carService.CreateCarAsync(car, Guid.Parse(userId));
+            return CreatedAtAction(nameof(GetCarById), new { id = createdCar.Id }, createdCar);
         }
     }
 }
